5c6c7fbdcd139331e476501040472fd125c36175
shubham singh <ss8451471@gmail.com>
2024-08-14 17:10:24 +0530

    version2

---
+++ lib/config/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(255, 18, 187, 173);
+const secondryColor = Color.fromARGB(255, 224, 110, 18);
+const backgroudColor = Color(0xFFFFFFFF);
+const fontColor = Color(0xFF000000);
+const lebelColor = Color(0xFF6B6B6B);
+const secondLebelColor = Color(0xffC6C6C6);
